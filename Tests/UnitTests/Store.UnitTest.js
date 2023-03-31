a99af7417ee914902ba097559c47f5c4d7d9bb60
Ramanathan <c.ramanathan23@gmail.com>
2023-03-31 12:24:35 +0530

    Update Store.UnitTest.js

--- Tests/UnitTests/Store.UnitTest.js
+++ Tests/UnitTests/Store.UnitTest.js
@@ -5,11 +5,11 @@ var store = new Store();
 
 describe( "Store Unit Tests", () => {
 	
-    it("Store should return zero objects when initialized", () => {
+   /* it("Store should return zero objects when initialized", () => {
         assert.equal(store.getAll().length == 0, true);
     });
 
-   /* it("Store should push objects when requested", () => {
+    it("Store should push objects when requested", () => {
         store.push({});
         assert.equal(store.getAll().length > 0, true);
     });
